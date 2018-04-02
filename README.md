8e120657fa418b68f967968c051cbb68e31b59c0
chris <chrishaig14@gmail.com>
2018-04-02 15:11:36 -0300

    Instrucciones para instalar y compilar MPI. Hello world MPI

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# cluster
-TP Final - Sistemas Distribuidos - FIUBA
+# TP Final: Cluster MPI - Sistemas Distribuidos - FIUBA
 
-Tutorial de MPI: http://mpitutorial.com/tutorials/
+Tutorial de MPI:
+
+http://mpitutorial.com/tutorials/
+
+## Instalar MPI:
+
+Descargar OpenMPI: https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.1.tar.gz
+
+```
+gunzip -c openmpi-3.0.1.tar.gz | tar xf -
+cd openmpi-3.0.1
+./configure
+sudo make all install
+```
+
+Compilar con 
+
+`mpicc -o <ejecutable> <codigo>`
+
+Ejecutar con
+
+`mpirun -np <nro procesos> <ejecutable>`
+
+o si hay un hostfile
+
+`mpirun --hostfile <hostfile> -np <nro procesos> <ejecutable>`
---
+++ hello_world.c
@@ -0,0 +1,28 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Inicializar MPI
+    MPI_Init(NULL, NULL);
+
+    // Obtener el numero de procesos
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Obtener el rank (id) del proceso actual
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Obtener el nombre del procesador actual
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    printf("Hello world from processor %s, rank %d"
+           " out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    // Finalizar MPI
+    MPI_Finalize();
+    
+}
